a7fe79e91740f51193cc9e610d4a825313d97e53
Chinseok Lee <me@askcompany.kr>
2019-10-08 13:33:12 +0900

    blog앱에 post_list 뷰 구현

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request):  # HttpRequest 타입
+    message = "Welcome to the Django World."
+    return HttpResponse("<h1>Hello</h1>" + message)
